b3f25a0dc23041e133b1b357dff596b6ac6c84c2
seeu359 <a.ch3re359@yandex.ru>
2022-09-10 22:23:53 +0300

    Make first jobs. Tests was added

---
+++ tests/test_classes_and_object.py
@@ -0,0 +1,22 @@
+from part_1.classes_and_objects import database
+from part_1.classes_and_objects import goods
+from part_1.classes_and_objects import car
+
+
+def test_database():
+    assert database.DataBase.pk == 1
+    assert database.DataBase.title == 'Классы и объекты'
+    assert database.DataBase.author == 'Сергей Балакирев'
+    assert database.DataBase.views == 14356
+    assert database.DataBase.comments == 12
+
+
+def test_goods():
+    assert goods.Goods.weight == 154
+    assert goods.Goods.price == 2048
+    assert goods.Goods.inflation == 100
+
+
+def test_car():
+    assert car.Car.model == 'Тойота'
+    assert car.Car.color == 'Розовый'
